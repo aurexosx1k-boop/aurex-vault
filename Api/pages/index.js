07d83f61c0b0a488979976b566b3441ff05a5fad
aurexosx1k-boop <aurexosx1k@gmail.com>
2025-12-27 21:38:10 -0500

    Create index.js

---
+++ Api/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>AUREX™</h1>
+      <p>Your Data. Your Dividends.</p>
+      <button>Enter Vault</button>
+    </div>
+  );
+}
